ffb89888cee61d5ff10cb9e47702f61bbaa85e46
j-hyodo <127344498+j-hyodo@users.noreply.github.com>
2023-08-21 16:53:14 -0700

    Update ftrace_cmd_latency.py

--- ftrace_cmd_latency.py
+++ ftrace_cmd_latency.py
@@ -3,5 +3,6 @@ import argparse
 parser = argparse.ArgumentParser(description="ftrace command latency", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-f", "--file",help="fTrace log file")
 args = parser.parse_args()
-
+config = vars (args)
+print (config)
 
